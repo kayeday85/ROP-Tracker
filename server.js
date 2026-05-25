4804756c5e1ece8a9c5bf7dc1ad0b97e112b3214
kayeday85 <kayelynndaylyons21@gmail.com>
2026-05-25 13:02:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,12 +12,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB error:", err));
-
 // Routes
 app.use("/auth", authRoutes);
 
