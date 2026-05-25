cd650ee0c876b94bfd729aadaf8aba292e051a51
kayeday85 <kayelynndaylyons21@gmail.com>
2026-05-25 08:36:38 -0700

    Update route.js

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -6,16 +6,22 @@ import bcrypt from "bcryptjs";
 export async function POST(req) {
   await connectDB();
 
-  const { name, email, password } = await req.json();
+  const { email, password } = await req.json();
 
   try {
-    const passwordHash = await bcrypt.hash(password, 10);
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
 
-    const user = await User.create({ name, email, passwordHash });
+    const isMatch = await bcrypt.compare(password, user.passwordHash);
+    if (!isMatch) {
+      return NextResponse.json({ message: "Invalid password" }, { status: 401 });
+    }
 
     const res = NextResponse.json(
-      { message: "User registered", user },
-      { status: 201 }
+      { message: "Login successful", user },
+      { status: 200 }
     );
 
     // CORS HEADERS
