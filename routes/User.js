b260110629ee53513aa1b37558b87ec43d721f46
kayeday85 <kayelynndaylyons21@gmail.com>
2026-05-25 10:58:12 -0700

    Delete routes directory

--- routes/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const UserSchema = new mongoose.Schema({
-  name: { type: String },
-  email: { type: String, unique: true },
-  passwordHash: { type: String },
-});
-
-export default mongoose.model("User", UserSchema);
