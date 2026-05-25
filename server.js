8a756096c0705638d461f5c4eb77c51832db7b68
kayeday85 <kayelynndaylyons21@gmail.com>
2026-05-25 13:25:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ dotenv.config();
 console.log("MONGODB_URI from env:", process.env.MONGODB_URI);
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./lib/db.js";
 import mongoose from "mongoose";
 import authRoutes from "./routes/auth.js";
 
@@ -17,4 +18,6 @@ app.use("/auth", authRoutes);
 
 // Start server
 const PORT = process.env.PORT || 3000;
+await connectDB();
+console.log("Connected to MongoDB");
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
